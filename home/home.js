d6ae17f891a00328a46343876495a6631b4834ad
Tassio Hipólito <tassio.h.a@gmail.com>
2023-05-31 02:45:18 -0300

    adicionando responsividade na navbar

--- home/home.js
+++ home/home.js
@@ -1,5 +1,5 @@
 
-
+/* code carrossel*/
 var radio = document.querySelector('.manual-btn')
 var contador = 1
 
@@ -18,3 +18,35 @@ function passarSlide() {
 
     document.getElementById('radio'+contador).checked = true;
 }
+
+/*FIM code carrossel*/
+
+/* code esconder/mostrar classe*/
+
+var barras = document.querySelector(".barras-burguer");
+var x = document.querySelector(".img-X-hamburguer")
+var menu = document.querySelector(".hamburguer");
+var fade_hamb = document.getElementById("fade_hamburguer");
+
+barras.addEventListener('click', function(){
+    menu.classList.toggle('showmenu');
+    fade_hamb.style.display ="block";
+});
+
+fade_hamb.addEventListener('click', function(){
+    menu.classList.toggle('showmenu');
+    fade_hamb.style.display ="";
+});
+
+x.addEventListener('click', function() {
+    menu.classList.toggle("showmenu");
+    fade_hamb.style.display ="";
+    
+})
+
+
+
+
+
+
+/*FIM code esconder/mostrar classe*/
