c2cc47817389cef5203e9e76b2876349017440eb
Tassio Hipólito <tassio.h.a@gmail.com>
2023-06-12 07:45:13 -0300

    adicionando todos os planos com botões funcionando

--- home/home.js
+++ home/home.js
@@ -23,29 +23,52 @@ function passarSlide() {
 
 
 /* esconder info planos */
-var divInfo  = document.getElementById("info");
-var btn = document.getElementById("btn-mostra-esconde");
+var divInfoS  = document.getElementById("infoS");
+var divInfoA  = document.getElementById("infoA");
+var divInfoM  = document.getElementById("infoM");
+var btn1 = document.getElementById("btn-mostra-esconde-semestral");
+var btn2 = document.getElementById("btn-mostra-esconde-anoal");
+var btn3 = document.getElementById("btn-mostra-esconde-mensal");
 
-btn.addEventListener("click", function() {
-    
-    console.log('jj');
-    divInfo.classList.toggle("esconde");
-    
-    
+btn1.addEventListener("click", function() {
+    divInfoS.classList.toggle("esconde");
 });
 
 
+function mudarTextobtnS() {
+    if (btn1.textContent === "Ver mais") {
+        btn1.innerHTML= "Ver menos";
+    }else{
+        btn1.innerHTML= "Ver mais";
+    }
+    
+};
 
 
-// function mostrar() {
-//     if (div.style.display == "none") {
-//         console.log('ojji');
-//         div.style.display = "flex";
-//     }else{
-//         div.style.display = "none";
-//         console.log('nao');
-//     }
-// };
+btn2.addEventListener("click", function() {
+    divInfoA.classList.toggle("esconde");
+});
+
+function mudarTextobtnA() {
+    if (btn2.textContent === "Ver mais") {
+        btn2.innerHTML= "Ver menos";
+    }else{
+        btn2.innerHTML= "Ver mais";
+    }
+    
+};
+
+btn3.addEventListener("click", function() {
+    divInfoM.classList.toggle("esconde");
+});
+
+function mudarTextobtnM() {
+    if (btn3.textContent === "Ver mais") {
+        btn3.innerHTML= "Ver menos";
+    }else{
+        btn3.innerHTML= "Ver mais";
+    }
+};
 
 
 /* FIM esconder info planos */
